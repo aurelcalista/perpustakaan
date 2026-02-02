3319a09180370f626157a46666e59d22aed9f529
livymeii <adindaameiliaa09@gmail.com>
2026-02-02 10:01:55 +0700

    p

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,12 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', function () {
     return view('home');
 });
+
+// Route::get('/home', function () {
+//     return view('home');
+// });
 
 
 Route::get('/dashboard', function () {
