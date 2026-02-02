ec78779d15aa8419d4d1e6385e2224da38009953
livymeii <adindaameiliaa09@gmail.com>
2026-02-02 10:16:13 +0700

    p

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,6 @@ Route::get('/', function () {
     return view('home');
 });
 
-// Route::get('/home', function () {
-//     return view('home');
-// });
-
 
 Route::get('/dashboard', function () {
     return view('dashboard');
