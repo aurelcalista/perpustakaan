4bc9e1dcdc7266b826e030d8b0012e85404a89dd
aurelcalista <arcaess1129@gmail.com>
2026-02-02 08:32:01 +0700

    p

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('partials.header')
+</head>
+<body>
+    @include('partials.navbar')
+    @yield('content')
+    
+    @include('partials.footer')
+</body>
+</html>
