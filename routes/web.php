23bf99617825d2c5e1565a8809af3f7da1045558
unknown <gniiputri031@gmail.com>
2026-02-06 09:08:21 +0700

    set frontend home route

--- routes/web.php
+++ routes/web.php
@@ -1,40 +1,52 @@
 <?php
 
-
-use App\Http\Controllers\ProfileController;
-use App\Models\Category;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DashboardPetugasController;
 
-
+/*
+|--------------------------------------------------------------------------
+| FRONTEND (AGNIA)
+|--------------------------------------------------------------------------
+*/
 Route::get('/', function () {
-    return view('home');
-});
-Route::get('/dashboard_admin', function () {
-    return view('dashboard_admin.index');
+    return view('frontend.home');
 });
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard_petugas', [DashboardPetugasController::class, 'index'])->name('dashboard_petugas');
-});
-
-
-
+/*
+|--------------------------------------------------------------------------
+| AUTH
+|--------------------------------------------------------------------------
+*/
 Route::get('/login', function () {
     return view('auth.login');
 });
 
-
-
-
+/*
+|--------------------------------------------------------------------------
+| DASHBOARD
+|--------------------------------------------------------------------------
+*/
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard_petugas', [DashboardPetugasController::class, 'index'])
+        ->name('dashboard_petugas');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+/*
+|--------------------------------------------------------------------------
+| ADMIN
+|--------------------------------------------------------------------------
+*/
+Route::get('/dashboard_admin', function () {
+    return view('dashboard_admin.index');
 });
 
 require __DIR__.'/auth.php';
