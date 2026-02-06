b80c0f2438c8ba4b7e12814b073781b594ae16da
aurelcalista <arcaess1129@gmail.com>
2026-02-06 10:41:35 +0700

    perbaikan web

--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,12 @@ use App\Http\Controllers\Auth\AdminAuthController;
 use App\Http\Controllers\DashboardAdminController;
 use App\Http\Controllers\DashboardPetugasController;
 
-/*
-|--------------------------------------------------------------------------
-| Public
-|--------------------------------------------------------------------------
-*/
+
 Route::get('/', function () {
     return view('home');
 });
 
-/*
-|--------------------------------------------------------------------------
-| Admin Login (Guest)
-|--------------------------------------------------------------------------
-*/
+
 Route::middleware('guest')->group(function () {
     Route::get('/admin/login', [AdminAuthController::class, 'create'])
         ->name('admin.login');
@@ -28,42 +20,26 @@ Route::middleware('guest')->group(function () {
         ->name('admin.login.store');
 });
 
-/*
-|--------------------------------------------------------------------------
-| Admin
-|--------------------------------------------------------------------------
-*/
+
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', [DashboardAdminController::class, 'index'])
         ->name('admin.dashboard');
 });
 
-/*
-|--------------------------------------------------------------------------
-| Petugas
-|--------------------------------------------------------------------------
-*/
+
 Route::middleware(['auth', 'role:petugas'])->group(function () {
     Route::get('/petugas/dashboard', [DashboardPetugasController::class, 'index'])
         ->name('petugas.dashboard');
 });
 
-/*
-|--------------------------------------------------------------------------
-| Siswa
-|--------------------------------------------------------------------------
-*/
+
 Route::middleware(['auth', 'role:siswa'])->group(function () {
     Route::get('/siswa/home', function () {
         return view('siswa.home');
     })->name('siswa.home');
 });
 
-/*
-|--------------------------------------------------------------------------
-| Profile
-|--------------------------------------------------------------------------
-*/
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
