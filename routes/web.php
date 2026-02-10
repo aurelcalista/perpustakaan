545c95157310b432d7869cff0ea271c1944a021e
unknown <gniiputri031@gmail.com>
2026-02-10 11:04:21 +0700

    resolve conflict route & tambah halaman detail buku

--- routes/web.php
+++ routes/web.php
@@ -1,76 +1,73 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Auth\AdminAuthController;
 use App\Http\Controllers\DashboardAdminController;
 use App\Http\Controllers\DashboardPetugasController;
-use App\Http\Controllers\BukuController;
 
-
-
+/*
+|--------------------------------------------------------------------------
+| PUBLIC
+|--------------------------------------------------------------------------
+*/
 Route::get('/', function () {
     return view('home');
-})->name('home');
+});
 
 Route::get('/buku/detail', function () {
     return view('frontend.detail-buku');
-});
+})->name('buku.detail');
 
-
-
+/*
+|--------------------------------------------------------------------------
+| ADMIN LOGIN
+|--------------------------------------------------------------------------
+*/
 Route::get('/admin/login', [AdminAuthController::class, 'create'])
     ->name('admin.login');
 
 Route::post('/admin/login', [AdminAuthController::class, 'store'])
     ->name('admin.login.store');
 
-
-Route::middleware(['auth', 'role:admin'])
-    ->prefix('admin')
-    ->name('admin.')
-    ->group(function () {
-
-        Route::get('/dashboard', [DashboardAdminController::class, 'index'])
-            ->name('dashboard');
-
-        Route::controller(BukuController::class)
-            ->prefix('buku')
-            ->name('buku.')
-            ->group(function () {
-                Route::get('/', 'index')->name('index');
-                Route::get('/create', 'create')->name('create');
-                Route::post('/', 'store')->name('store');
-                Route::get('/{id}/edit', 'edit')->name('edit');
-                Route::put('/{id}', 'update')->name('update');
-                Route::delete('/{id}', 'destroy')->name('destroy');
-            });
-
-        Route::post('/logout', [AdminAuthController::class, 'destroy'])
-            ->name('logout');
+/*
+|--------------------------------------------------------------------------
+| ADMIN
+|--------------------------------------------------------------------------
+*/
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin/dashboard', [DashboardAdminController::class, 'index'])
+        ->name('admin.dashboard');
 });
 
-
+/*
+|--------------------------------------------------------------------------
+| PETUGAS
+|--------------------------------------------------------------------------
+*/
 Route::middleware(['auth', 'role:petugas'])->group(function () {
     Route::get('/petugas/dashboard', [DashboardPetugasController::class, 'index'])
         ->name('petugas.dashboard');
 });
 
-
-
+/*
+|--------------------------------------------------------------------------
+| SISWA
+|--------------------------------------------------------------------------
+*/
 Route::middleware(['auth', 'role:siswa'])->group(function () {
-    Route::get('/home', function () {
-        return view('home');
+    Route::get('/siswa/home', function () {
+        return view('siswa.home');
     })->name('siswa.home');
 });
 
-
+/*
+|--------------------------------------------------------------------------
+| PROFILE
+|--------------------------------------------------------------------------
+*/
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
-    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
