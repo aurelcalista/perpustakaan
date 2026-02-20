88c313daaf60771d86342a4dc067cb92401fcc24
aurelcalista <arcaess1129@gmail.com>
2026-02-20 14:19:43 +0700

    perbaikan navbar

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -301,6 +301,17 @@
         }
     }
     </script>
+    <script>
+        // Navbar scroll effect
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = 'var(--secondary-color)';
+    } else {
+        navbar.style.backgroundColor = 'transparent';
+    }
+});
+    </script>
 
     @yield('scripts')
 </body>
