d501c1f163d4cc3e949ee01dac4fe05acb342d46
aurelcalista <arcaess1129@gmail.com>
2026-03-25 21:42:58 +0700

    hapus akun

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="id">
-<head>
+<h>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -231,7 +231,7 @@
       footer.new-footer .footer-inner { grid-template-columns: 1fr; }
     }
   </style>
-
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
   @stack('styles')
 </head>
 <body class="@yield('body-class')">
