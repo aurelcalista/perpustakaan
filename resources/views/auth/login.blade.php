c665d5de061273404cf24093dda326e13b248365
kezhiaaurelia <kezhiaaurelia3@gmail.com>
2026-03-31 12:29:18 +0700

    benerin form login register (arel)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,6 @@
 @extends('layout.login')
 @section('content')
 
-
 <div class="login-container">
     <div class="logo-container">
         <div class="logo">
@@ -33,13 +32,21 @@
             </div>
 
             <div class="form-group">
-                <label for="password">Password</label>
-                <div class="input-wrapper">
+                <label for="password">Kata Sandi</label>
+                <div class="input-wrapper input-pw-wrap">
                     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9V6zm9 14H6V10h12v10zm-6-3c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"/>
                     </svg>
                     <input type="password" id="password" name="password"
-                           placeholder="Masukkan password" required>
+                           class="form-input" placeholder="Masukkan password" required>
+                    <button type="button" class="toggle-pw" onclick="togglePassword('password', this)" tabindex="-1">
+                        <svg class="eye-show" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zm0 12.5c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/>
+                        </svg>
+                        <svg class="eye-hide" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style="display:none">
+                            <path d="M12 7c2.76 0 5 2.24 5 5 0 .65-.13 1.26-.36 1.83l2.92 2.92c1.51-1.26 2.7-2.89 3.43-4.75-1.73-4.39-6-7.5-11-7.5-1.4 0-2.74.25-3.98.7l2.16 2.16C10.74 7.13 11.35 7 12 7zM2 4.27l2.28 2.28.46.46C3.08 8.3 1.78 10.02 1 12c1.73 4.39 6 7.5 11 7.5 1.55 0 3.03-.3 4.38-.84l.42.42L19.73 22 21 20.73 3.27 3 2 4.27zM7.53 9.8l1.55 1.55c-.05.21-.08.43-.08.65 0 1.66 1.34 3 3 3 .22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53-2.76 0-5-2.24-5-5 0-.79.2-1.53.53-2.2zm4.31-.78l3.15 3.15.02-.16c0-1.66-1.34-3-3-3l-.17.01z"/>
+                        </svg>
+                    </button>
                 </div>
             </div>
         </div>
@@ -66,7 +73,16 @@
         </div>
 
     </form>
-    
 </div>
+
+<script>
+function togglePassword(fieldId, btn) {
+    const input = document.getElementById(fieldId);
+    const isPassword = input.type === 'password';
+    input.type = isPassword ? 'text' : 'password';
+    btn.querySelector('.eye-show').style.display = isPassword ? 'none' : 'block';
+    btn.querySelector('.eye-hide').style.display = isPassword ? 'block' : 'none';
+}
+</script>
 
 @endsection
