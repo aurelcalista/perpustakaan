d27a717ee91c4439350752b101b6d535033a8754
kezhiaaurelia <kezhiaaurelia3@gmail.com>
2026-03-31 13:11:48 +0700

    perbaikan hal login register lagi (arel)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,51 +18,44 @@
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
-        <div class="form-row">
-            <div class="form-group">
-                <label for="nis">ID Anggota (NIS)</label>
-                <div class="input-wrapper">
-                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M20 4H4c-1.11 0-1.99.89-1.99 2L2 18c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V6c0-1.11-.89-2-2-2zm0 14H4v-6h16v6zm0-10H4V6h16v2z"/>
-                    </svg>
-                    <input type="text" id="nis" name="nis"
-                           value="{{ old('nis') }}"
-                           placeholder="Masukkan NIS Anda" required autofocus>
-                </div>
+        <div class="form-group">
+            <label for="nis">ID Anggota (NIS)</label>
+            <div class="input-wrapper">
+                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20 4H4c-1.11 0-1.99.89-1.99 2L2 18c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V6c0-1.11-.89-2-2-2zm0 14H4v-6h16v6zm0-10H4V6h16v2z"/>
+                </svg>
+                <input type="text" id="nis" name="nis"
+                       value="{{ old('nis') }}"
+                       placeholder="Masukkan NIS Anda" required autofocus>
             </div>
+        </div>
 
-            <div class="form-group">
-                <label for="password">Kata Sandi</label>
-                <div class="input-wrapper input-pw-wrap">
-                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9V6zm9 14H6V10h12v10zm-6-3c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"/>
+        <div class="form-group">
+            <label for="password">Kata Sandi</label>
+            <div class="input-wrapper input-pw-wrap">
+                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9V6zm9 14H6V10h12v10zm-6-3c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"/>
+                </svg>
+                <input type="password" id="password" name="password"
+                       class="form-input" placeholder="Masukkan password" required>
+                <button type="button" class="toggle-pw" onclick="togglePassword('password', this)" tabindex="-1">
+                    <svg class="eye-show" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zm0 12.5c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/>
                     </svg>
-                    <input type="password" id="password" name="password"
-                           class="form-input" placeholder="Masukkan password" required>
-                    <button type="button" class="toggle-pw" onclick="togglePassword('password', this)" tabindex="-1">
-                        <svg class="eye-show" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zm0 12.5c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/>
-                        </svg>
-                        <svg class="eye-hide" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style="display:none">
-                            <path d="M12 7c2.76 0 5 2.24 5 5 0 .65-.13 1.26-.36 1.83l2.92 2.92c1.51-1.26 2.7-2.89 3.43-4.75-1.73-4.39-6-7.5-11-7.5-1.4 0-2.74.25-3.98.7l2.16 2.16C10.74 7.13 11.35 7 12 7zM2 4.27l2.28 2.28.46.46C3.08 8.3 1.78 10.02 1 12c1.73 4.39 6 7.5 11 7.5 1.55 0 3.03-.3 4.38-.84l.42.42L19.73 22 21 20.73 3.27 3 2 4.27zM7.53 9.8l1.55 1.55c-.05.21-.08.43-.08.65 0 1.66 1.34 3 3 3 .22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53-2.76 0-5-2.24-5-5 0-.79.2-1.53.53-2.2zm4.31-.78l3.15 3.15.02-.16c0-1.66-1.34-3-3-3l-.17.01z"/>
-                        </svg>
-                    </button>
-                </div>
+                    <svg class="eye-hide" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style="display:none">
+                        <path d="M12 7c2.76 0 5 2.24 5 5 0 .65-.13 1.26-.36 1.83l2.92 2.92c1.51-1.26 2.7-2.89 3.43-4.75-1.73-4.39-6-7.5-11-7.5-1.4 0-2.74.25-3.98.7l2.16 2.16C10.74 7.13 11.35 7 12 7zM2 4.27l2.28 2.28.46.46C3.08 8.3 1.78 10.02 1 12c1.73 4.39 6 7.5 11 7.5 1.55 0 3.03-.3 4.38-.84l.42.42L19.73 22 21 20.73 3.27 3 2 4.27zM7.53 9.8l1.55 1.55c-.05.21-.08.43-.08.65 0 1.66 1.34 3 3 3 .22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53-2.76 0-5-2.24-5-5 0-.79.2-1.53.53-2.2zm4.31-.78l3.15 3.15.02-.16c0-1.66-1.34-3-3-3l-.17.01z"/>
+                    </svg>
+                </button>
             </div>
         </div>
 
         <div class="form-options">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox"
-                       class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
-                       name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Ingat saya') }}</span>
+            <label class="inline-flex items-center" style="display:flex; align-items:center; gap:8px; cursor:pointer;">
+                <input id="remember_me" type="checkbox" name="remember" style="accent-color:#667eea;">
+                <span>Ingat saya</span>
             </label>
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                   href="{{ route('password.request') }}">
-                    {{ __('Lupa kata sandi?') }}
-                </a>
+                <a href="{{ route('password.request') }}">Lupa kata sandi?</a>
             @endif
         </div>
 
@@ -71,7 +64,6 @@
         <div class="register-link">
             Belum punya akun? <a href="{{ route('register') }}">Daftar Sekarang</a>
         </div>
-
     </form>
 </div>
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,55 +1,55 @@
 @extends('layout.login')
 @section('content')
 
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
-        <div class="login-container">
-            <div class="logo-container">
-                <div class="logo">
-                    <img src="{{ asset('images/logosmk.png') }}" alt="logo" class="img-thumbnail rounded-circle" width="150">
+<form method="POST" action="{{ route('register') }}">
+    @csrf
+    <div class="register-container">
+        <div class="logo-container">
+            <div class="logo">
+                <img src="{{ asset('images/logosmk.png') }}" alt="logo" class="img-thumbnail rounded-circle" width="150">
+            </div>
+            <h1 class="school-name">SMK Negeri 1 Cirebon</h1>
+            <p class="library-title">Sistem Informasi Perpustakaan</p>
+        </div>
+
+        <div class="welcome-text">
+            <h2>Selamat Datang</h2>
+            <p>Silakan registrasi untuk mengakses perpustakaan digital</p>
+        </div>
+
+        <div class="form-grid">
+
+            <div class="form-group">
+                <label for="nis">NIS</label>
+                <div class="input-wrapper">
+                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M20 4H4c-1.11 0-1.99.89-1.99 2L2 18c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V6c0-1.11-.89-2-2-2zm0 14H4v-6h16v6zm0-10H4V6h16v2z"/>
+                    </svg>
+                    <input type="text" id="nis" name="nis" placeholder="Masukkan NIS Anda" required>
                 </div>
-                <h1 class="school-name">SMK Negeri 1 Cirebon</h1>
-                <p class="library-title">Sistem Informasi Perpustakaan</p>
             </div>
 
-            <div class="welcome-text">
-                <h2>Selamat Datang</h2>
-                <p>Silakan registrasi untuk mengakses perpustakaan digital</p>
+            <div class="form-group">
+                <label for="nama">Nama Anggota</label>
+                <div class="input-wrapper">
+                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/>
+                    </svg>
+                    <input type="text" id="nama" name="nama" placeholder="Masukkan Nama Anda" required>
+                </div>
             </div>
 
-            <div class="form-row">
-                <div class="form-group">
-                    <label for="nis">NIS</label>
-                    <div class="input-wrapper">
-                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M20 4H4c-1.11 0-1.99.89-1.99 2L2 18c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V6c0-1.11-.89-2-2-2zm0 14H4v-6h16v6zm0-10H4V6h16v2z"/>
-                        </svg>
-                        <input type="text" id="nis" name="nis" placeholder="Masukkan NIS Anda" required>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label for="nama">Nama Anggota</label>
-                    <div class="input-wrapper">
-                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/>
-                        </svg>
-                        <input type="text" id="nama" name="nama" placeholder="Masukkan Nama Anda" required>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label for="noidentitas">Nomor Identitas / Kelas</label>
-                    <div class="input-wrapper">
-                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm2 16H8v-2h8v2zm0-4H8v-2h8v2zm-3-5V3.5L18.5 9H13z"/>
-                        </svg>
-                        <select id="noidentitas" name="noidentitas" required>
-                            <option value=""> Pilih Kelas Anda </option>
-                            <option value="XI RPL 1" {{ old('noidentitas') == 'XI RPL 1' ? 'selected' : '' }}>XI RPL 1</option>
-                            <option value="XI RPL 2" {{ old('noidentitas') == 'XI RPL 2' ? 'selected' : '' }}>XI RPL 2</option>
-                        </select>
-                    </div>
+            <div class="form-group">
+                <label for="noidentitas">Nomor Identitas / Kelas</label>
+                <div class="input-wrapper">
+                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm2 16H8v-2h8v2zm0-4H8v-2h8v2zm-3-5V3.5L18.5 9H13z"/>
+                    </svg>
+                    <select id="noidentitas" name="noidentitas" required>
+                        <option value=""> Pilih Kelas Anda </option>
+                        <option value="XI RPL 1" {{ old('noidentitas') == 'XI RPL 1' ? 'selected' : '' }}>XI RPL 1</option>
+                        <option value="XI RPL 2" {{ old('noidentitas') == 'XI RPL 2' ? 'selected' : '' }}>XI RPL 2</option>
+                    </select>
                 </div>
             </div>
 
@@ -63,25 +63,23 @@
                 </div>
             </div>
 
-            <div class="form-row">
-                <div class="form-group">
-                    <label for="alamat">Alamat</label>
-                    <div class="input-wrapper">
-                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
-                        </svg>
-                        <input type="text" id="alamat" name="alamat" placeholder="Masukkan Alamat Anda" required>
-                    </div>
+            <div class="form-group">
+                <label for="alamat">Alamat</label>
+                <div class="input-wrapper">
+                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
+                    </svg>
+                    <input type="text" id="alamat" name="alamat" placeholder="Masukkan Alamat Anda" required>
                 </div>
+            </div>
 
-                <div class="form-group">
-                    <label for="notlp">No Telepon</label>
-                    <div class="input-wrapper">
-                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z"/>
-                        </svg>
-                        <input type="text" id="notlp" name="notlp" placeholder="Masukkan Nomor Telepon" required>
-                    </div>
+            <div class="form-group">
+                <label for="notlp">No Telepon</label>
+                <div class="input-wrapper">
+                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z"/>
+                    </svg>
+                    <input type="text" id="notlp" name="notlp" placeholder="Masukkan Nomor Telepon" required>
                 </div>
             </div>
 
@@ -123,22 +121,24 @@
                 </div>
             </div>
 
-            <!-- Privacy Policy Checkbox -->
-            <div style="margin-top: 1.5rem;">
-                <label style="display: flex; align-items: start; gap: 0.5rem; cursor: pointer; font-size: 0.875rem;">
-                    <input type="checkbox" required style="margin-top: 0.25rem;">
+            <div class="full-width" style="margin-top: 0.5rem;">
+                <label style="display: flex; align-items: flex-start; gap: 0.5rem; cursor: pointer; font-size: 0.875rem; color: #64748b;">
+                    <input type="checkbox" required style="margin-top: 0.2rem; accent-color: #667eea;">
                     <span>Saya menyatakan telah membaca dan menyetujui terkait <a href="#" style="color: #5ac4c2; text-decoration: none; font-weight: 600;">Kebijakan Privasi</a></span>
                 </label>
             </div>
-            <br>
 
-            <button type="submit" class="login-button">Register</button>
+            <div class="full-width" style="margin-top: 1rem;">
+                <button type="submit" class="login-button">Register</button>
+            </div>
 
-            <div class="register-link">
+            <div class="register-link full-width">
                 Sudah punya akun? <a href="{{ route('login') }}">Login</a>
             </div>
+
         </div>
-    </form>
+    </div>
+</form>
 
 <script>
 function togglePassword(fieldId, btn) {
